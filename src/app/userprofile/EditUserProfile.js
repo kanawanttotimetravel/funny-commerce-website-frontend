3700d960239f55839b3b0a0ecbb3932dc26a95e3
minh <mlodt3@gmail.com>
2024-05-21 04:24:07 +0700

    create EditableField component, refactor userprofile
    create EditableField component, refactor userprofile

--- src/app/userprofile/EditUserProfile.js
+++ src/app/userprofile/EditUserProfile.js
@@ -1,58 +1,38 @@
 "use client";
 
+import EditableField from '@/components/editablefield/EditableField';
 import React, { useState } from 'react';
 
 const EditUserProfile = ({ user, onSave }) => {
-  const [formData, setFormData] = useState({ ...user });
+    const [formData, setFormData] = useState({ ...user });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+        ...formData,
+        [name]: value,
+        });
+    };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onSave(formData);
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onSave(formData);
+    };
 
-  return (
-    <form className="edit-profile-form" onSubmit={handleSubmit}>
-        <div>
-            <label>First Name:</label>
-            <input type="text" name="firstName" value={formData.firstName} onChange={handleChange} />
-        </div>
-        <div>
-            <label>Last Name:</label>
-            <input type="text" name="lastName" value={formData.lastName} onChange={handleChange} />
-        </div>
-        <div>
-            <label>Phone Number:</label>
-            <input type="text" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange} />
-        </div>
-        <div>
-            <label>Email:</label>
-            <input type="email" name="email" value={formData.email} onChange={handleChange} />
-        </div>
-        <div>
-            <label>Country / Region:</label>
-            <input type="text" name="country" value={formData.country} onChange={handleChange} />
-        </div>
-        <div>
-            <label>Street:</label>
-            <input type="text" name="street" value={formData.street} onChange={handleChange} />
-        </div>
-        <div>
-            <label>Town / City:</label>
-            <input type="text" name="city" value={formData.city} onChange={handleChange} />
-        </div>
-        <div className="save-button-container">
-            <button type="submit" className="save-button">Save</button>
-        </div>
-    </form>
-  );
+    return (
+        <form className="edit-profile-form" onSubmit={handleSubmit}>
+            <EditableField label="First Name" name="firstName" value={formData.firstName} onChange={handleChange} />
+            <EditableField label="Last Name" name="lastName" value={formData.lastName} onChange={handleChange} />
+            <EditableField label="Phone Number" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange} />
+            <EditableField label="Email" name="email" value={formData.email} onChange={handleChange} />
+            <EditableField label="Country / Region" name="country" value={formData.country} onChange={handleChange} />
+            <EditableField label="Street" name="street" value={formData.street} onChange={handleChange} />
+            <EditableField label="Town / City" name="city" value={formData.city} onChange={handleChange} />
+            <div className="save-button-container">
+                <button type="submit" className="save-button">Save</button>
+            </div>
+        </form>
+    );
 };
 
 export default EditUserProfile;
