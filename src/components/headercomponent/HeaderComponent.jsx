bda73ff3fef16aa3171700472509a6be502c29e0
minh <mlodt3@gmail.com>
2024-05-23 21:35:30 +0700

    update header -> fixed
    update header -> fixed

--- src/components/headercomponent/HeaderComponent.jsx
+++ src/components/headercomponent/HeaderComponent.jsx
@@ -15,51 +15,53 @@ import {useRouter} from "next/navigation";
 function Header() {
     const router = useRouter()
     return (
-        <div className="header">
-            <div className="logo">
-                <Link href={'/'} >
-                    Drakon
-                </Link>
-            </div>
-            <div className="hcenter">
-                <ul className="hlist">
+        <div className="header-wrapper">
+            <div className="header">
+                <div className="logo">
+                    <Link href={'/'} >
+                        Drakon
+                    </Link>
+                </div>
+                <div className="hcenter">
+                    <ul className="hlist">
 
-                    <li><Link href={'/'} className="text-wrapper">Home</Link></li>
-                    <li><Link href={'/shop'} className="text-wrapper">Shop</Link></li>
-                    <li><Link href={'/about'} className="text-wrapper">About</Link></li>
-                    <li><Link href={'/contact'} className="text-wrapper">Contact</Link></li>
+                        <li><Link href={'/'} className="text-wrapper">Home</Link></li>
+                        <li><Link href={'/shop'} className="text-wrapper">Shop</Link></li>
+                        <li><Link href={'/about'} className="text-wrapper">About</Link></li>
+                        <li><Link href={'/contact'} className="text-wrapper">Contact</Link></li>
 
-                </ul>
+                    </ul>
+                </div>
+                <div className="hright">
+                    <ul className="hlist">
+                        <li>
+                            <Link href={'/alerts'}>
+                                <AlertOutlined className="icon" alt="alert" />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href={'/search'}>
+                                <SearchOutlined className="icon" alt="search" />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href={'/favorites'}>
+                                <HeartOutlined className="icon" alt="heart" />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href={'/cart'}>
+                                <ShoppingCartOutlined className="icon" alt="shopping" />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href={'/userprofile'}>
+                                <UserOutlined className="icon" alt="user" />
+                            </Link>
+                        </li>
+                    </ul>
+                </div> 
             </div>
-            <div className="hright">
-                <ul className="hlist">
-                    <li>
-                        <Link href={'/alerts'}>
-                            <AlertOutlined className="icon" alt="alert" />
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href={'/search'}>
-                            <SearchOutlined className="icon" alt="search" />
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href={'/favorites'}>
-                            <HeartOutlined className="icon" alt="heart" />
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href={'/cart'}>
-                            <ShoppingCartOutlined className="icon" alt="shopping" />
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href={'/userprofile'}>
-                            <UserOutlined className="icon" alt="user" />
-                        </Link>
-                    </li>
-                </ul>
-            </div> 
         </div>
     );
 };  
