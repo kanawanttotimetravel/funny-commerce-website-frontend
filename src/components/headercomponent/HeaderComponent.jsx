431272d25cb6ace25905760ad06ce29c92efd52b
minh <mlodt3@gmail.com>
2024-05-25 14:19:19 +0700

    update search

--- src/components/headercomponent/HeaderComponent.jsx
+++ src/components/headercomponent/HeaderComponent.jsx
@@ -12,6 +12,7 @@ import {
     UserOutlined
 } from '@ant-design/icons';
 import SearchHistoryItem from "./SearchHistoryItem";
+import ItemCard from "../atomic/ItemCard";
 
 function Header() {
     const [searchTerm, setSearchTerm] = useState("");
@@ -36,75 +37,84 @@ function Header() {
     };
 
     return (
-        <div className="header-wrapper">
-            <div className="header">
-                <div className="logo">
-                    <Link href={'/'}>
-                        Drakon
-                    </Link>
-                </div>
-                <div className="hcenter">
-                    <ul className="hlist">
-                        <li><Link href={'/'} className="text-wrapper">Home</Link></li>
-                        <li><Link href={'/shop'} className="text-wrapper">Shop</Link></li>
-                        <li><Link href={'/about'} className="text-wrapper">About</Link></li>
-                        <li><Link href={'/contact'} className="text-wrapper">Contact</Link></li>
-                    </ul>
-                </div>
-                <div className="hright">
-                    <ul className="hlist">
-                        <li>
-                            <Link href={'/alerts'}>
-                                <AlertOutlined className="icon" alt="alert" />
-                            </Link>
-                        </li>
-                        <li>
-                            <form className="search-box" onSubmit={handleSearchSubmit}>
-                                <div className="input-search-wrapper">
-                                    <input 
-                                        type="text" 
-                                        name="search" 
-                                        placeholder="Search for..." 
-                                        required 
-                                        className="input-search"
-                                        value={searchTerm}
-                                        onChange={(e) => setSearchTerm(e.target.value)}
-                                    />
-                                    <div className="search-history">
-                                        <ul className="search-history-list">
-                                            {searchHistory.map((item, index) => (
-                                                <SearchHistoryItem key={index} content={item} />
-                                            ))}
-                                        </ul>
+        <div>
+            <div className="header-wrapper">
+                <div className="header">
+                    <div className="logo">
+                        <Link href={'/'}>
+                            Drakon
+                        </Link>
+                    </div>
+                    <div className="hcenter">
+                        <ul className="hlist">
+                            <li><Link href={'/'} className="text-wrapper">Home</Link></li>
+                            <li><Link href={'/shop'} className="text-wrapper">Shop</Link></li>
+                            <li><Link href={'/about'} className="text-wrapper">About</Link></li>
+                            <li><Link href={'/contact'} className="text-wrapper">Contact</Link></li>
+                        </ul>
+                    </div>
+                    <div className="hright">
+                        <ul className="hlist">
+                            <li>
+                                <Link href={'/alerts'}>
+                                    <AlertOutlined className="icon" alt="alert" />
+                                </Link>
+                            </li>
+                            <li>
+                                <form className="search-box" onSubmit={handleSearchSubmit}>
+                                    <div className="input-search-wrapper">
+                                        <input 
+                                            type="text" 
+                                            name="search" 
+                                            placeholder="Search for..." 
+                                            required 
+                                            className="input-search"
+                                            value={searchTerm}
+                                            onChange={(e) => setSearchTerm(e.target.value)}
+                                        />
+                                        <div className="search-history">
+                                            <ul className="search-history-list">
+                                                {searchHistory.map((item, index) => (
+                                                    <SearchHistoryItem key={index} content={item} />
+                                                ))}
+                                            </ul>
+                                        </div>
                                     </div>
-                                </div>
-                                <button type="submit" className="btn-search">
-                                    <SearchOutlined alt="search" className="icon"/>
-                                </button>
-                            </form>
-                        </li>
-                        <li>
-                            <Link href={'/favorites'}>
-                                <HeartOutlined className="icon" alt="heart" />
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href={'/cart'}>
-                                <ShoppingCartOutlined className="icon" alt="shopping" />
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href={'/userprofile'}>
-                                <UserOutlined className="icon" alt="user" />
-                            </Link>
-                        </li>
-                    </ul>
+                                    <button type="submit" className="btn-search">
+                                        <SearchOutlined alt="search" className="icon"/>
+                                    </button>
+                                </form>
+                            </li>
+                            <li>
+                                <Link href={'/favorites'}>
+                                    <HeartOutlined className="icon" alt="heart" />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href={'/cart'}>
+                                    <ShoppingCartOutlined className="icon" alt="shopping" />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href={'/userprofile'}>
+                                    <UserOutlined className="icon" alt="user" />
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
             <div className="search-results">
                 <ul>
                     {searchResults.map((result, index) => (
-                        <li key={index}>{result.name}</li>
+                        // <li key={index}>{result.name}</li>
+                        <ItemCard
+                            key={index}
+                            imageSrc={result.image}
+                            itemName={result.product_name}
+                            itemType={result.info}
+                            price={result.price}
+                        />
                     ))}
                 </ul>
             </div>
